fef05045081ff53af3051439ea97d1c04ea6e11d
Arachnoid <101623522+MNRosyad@users.noreply.github.com>
2023-04-07 00:32:00 +0700

    styling

--- app/src/main/java/com/week7/maps/MapsActivity.java
+++ app/src/main/java/com/week7/maps/MapsActivity.java
@@ -6,7 +6,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,28 +21,32 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PointOfInterest;
 import com.week7.maps.databinding.ActivityMapsBinding;
+
+import java.util.Locale;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        com.week7.maps.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
@@ -51,13 +60,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng custom = new LatLng(-7.7752447, 110.3744223);
-        mMap.addMarker(new MarkerOptions().position(custom).title("Marker in Custom"));
+        mMap.addMarker(new MarkerOptions()
+                .position(custom)
+                .title("Marker in Custom")
+                .icon(bitmapDescriptor(getApplicationContext(), R.drawable.ic_location)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(custom, 20));
+
+        MapStyleOptions mapStyle = MapStyleOptions.loadRawResourceStyle(this, R.raw.bnw_style);
+        mMap.setMapStyle(mapStyle);
 
         setMapLongClick(mMap);
         setPoiClick(mMap);
@@ -71,6 +86,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -93,24 +109,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void setMapLongClick(final GoogleMap map) {
-
-        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(@NonNull LatLng latLng) {
-                map.addMarker(new MarkerOptions().position(latLng));
-            }
+        map.setOnMapLongClickListener(latLng -> {
+            String text = String.format(Locale.getDefault(),
+                    "Lat : %1$.5f, Long: %2$.5f",
+                    latLng.latitude,
+                    latLng.longitude);
+            map.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .snippet(text)
+                    .title(text)
+                    .icon(bitmapDescriptor(getApplicationContext(), R.drawable.ic_add_location)));
         });
     }
 
     private void setPoiClick(final GoogleMap map) {
-        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
-            @Override
-            public void onPoiClick(@NonNull PointOfInterest pointOfInterest) {
-                Marker poiMarker = mMap.addMarker(new MarkerOptions()
-                        .position(pointOfInterest.latLng)
-                        .title(pointOfInterest.name));
-                poiMarker.showInfoWindow();
-            }
+        map.setOnPoiClickListener(pointOfInterest -> {
+            Marker poiMarker = mMap.addMarker(new MarkerOptions()
+                    .position(pointOfInterest.latLng)
+                    .title(pointOfInterest.name)
+                    .icon(bitmapDescriptor(getApplicationContext(), R.drawable.ic_poi)));
+            assert poiMarker != null;
+            poiMarker.showInfoWindow();
         });
     }
 
@@ -121,19 +140,30 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mMap.setMyLocationEnabled(true);
         } else {
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    enableMyLocation();
-                    break;
-                }
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            }
         }
+    }
+
+    private BitmapDescriptor bitmapDescriptor(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        assert vectorDrawable != null;
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        vectorDrawable.draw(canvas);
+
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
