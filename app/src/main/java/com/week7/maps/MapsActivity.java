b9b02eaf77d10284dbb982cb4934bde0a4572039
Arachnoid <101623522+MNRosyad@users.noreply.github.com>
2023-04-07 00:47:35 +0700

    Fixing icon and clean syntax

--- app/src/main/java/com/week7/maps/MapsActivity.java
+++ app/src/main/java/com/week7/maps/MapsActivity.java
@@ -31,7 +31,6 @@ import com.week7.maps.databinding.ActivityMapsBinding;
 
 import java.util.Locale;
 
-
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -43,27 +42,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         com.week7.maps.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         LatLng custom = new LatLng(-7.7752447, 110.3744223);
         mMap.addMarker(new MarkerOptions()
                 .position(custom)
